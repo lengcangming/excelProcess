2b50db120366644ed539a612b06750d822131704
root <root>
2020-03-08 09:57:49 +0800

    测试提交

---
+++ src/main/java/www/mwj/App.java
@@ -0,0 +1,13 @@
+package www.mwj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
